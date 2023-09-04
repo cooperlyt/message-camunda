4c5f30dd348723e23d89a26e6d25d016b6542a15
cooper <lyt3131@163.com>
2023-09-04 20:03:51 +0800

    prod

--- work-message/src/main/java/cc/coopersoft/cloud/message/WorkStatus.java
+++ work-message/src/main/java/cc/coopersoft/cloud/message/WorkStatus.java
@@ -4,20 +4,25 @@ import java.util.EnumSet;
 
 public enum WorkStatus {
 
-  PREPARE, //准备中
-  PRIVATE, //私有的，只有本人或本组可以看到
+  PREPARE(false), //准备中
+  PRIVATE(false), //私有的，只有本人或本组可以看到
 
-  RUNNING, //运行中
+  RUNNING(false), //运行中
 
-  COMPLETED, //已完成
+  COMPLETED(true), //已完成
 
-  VALID, //已生效，但业务还未完成
-  REJECT, //业务被驳加
-  ABORT, //业务未完成，被中止
-  DELETED; //业务已完成，后被删除
+  VALID(true), //已生效，但业务还未完成
+  REJECT(false), //业务被驳加
+  ABORT(false), //业务未完成，被中止
+  DELETED(false); //业务已完成，后被删除
 
   static EnumSet<WorkStatus> create = EnumSet.of(PREPARE,PRIVATE,RUNNING,COMPLETED);
 
   static EnumSet<WorkStatus> change = EnumSet.of(VALID,COMPLETED,REJECT,ABORT,DELETED);
 
+  public boolean valid;
+
+  WorkStatus(boolean valid) {
+    this.valid = valid;
+  }
 }
